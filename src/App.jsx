fd40400548dc85b68a186d1cbfaaebd352521945
Anthony Gorski <anthony.developpeurweb@gmail.com>
2023-10-24 17:17:09 +0200

    optimisation du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import axios from "axios";
+import { useState } from "react";
+
+function getData(api, setter) {
+	axios
+		.get(api)
+		.then((response) => setter(response.data))
+		.catch((err) => console.error(err));
+}
 
 function App() {
-	axios
-		.get("https://api.chucknorris.io/jokes/random")
-		.then((response) => console.log(response.data));
+	const [joke, setJoke] = useState("");
+	const BASE_API = "https://api.chucknorris.io/jokes/random";
 
 	return (
 		<div>
 			<h1>Joke Chuck Norris</h1>
-			<p>ceci est ma joke</p>
-			<button>Une autre joke ? 🔁</button>
+			<p>{joke.value}</p>
+			<button onClick={() => getData(BASE_API, setJoke)}>
+				Une autre joke ? 🔁
+			</button>
 		</div>
 	);
 }
